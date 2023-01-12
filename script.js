d32fc91fb6a14e516d8b674e5a326c54b2813f8e
Andrii <77144111+AndriiShvaika@users.noreply.github.com>
2023-01-12 02:57:15 +0200

    Fixed all comments written in feedback

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const args = {
   widthAndHeight: 1000,
-  numOfColumnsAndRows: 6,
-  timeLimit: 10,
+  numOfColumns: 4,
+  numOfRows: 3,
+  timeLimit: 15,
   themeColor: "crimson",
   fontSize: 18,
 };
@@ -28,44 +29,115 @@ document.querySelector(".controls-container").style.backgroundColor =
 let cards;
 let interval;
 
+class Card {
+  firstCard = false;
+  secondCard = false;
+
+  constructor(numOfCards) {
+    this.numOfCards = numOfCards;
+  }
+
+  flipCardAnimation(card) {
+    anime({
+      targets: card,
+      scale: [{ value: 1 }, { value: 1 }, { value: 1, delay: 250 }],
+      rotateY: { value: "+=180", delay: 200 },
+      easing: "easeInOutSine",
+      duration: 400,
+      complete: function () {
+        !card.classList.contains("flipped")
+          ? card.classList.add("flipped")
+          : card.classList.remove("flipped");
+      },
+    });
+
+    return;
+  }
+
+  cardOnClickLogic = (e, stopGame) => {
+    e.preventDefault();
+    const card = e.target.closest(".card-container");
+    if (!card) return;
+    if (
+      !card.classList.contains("flipped") ||
+      !card.classList.contains("matched")
+    ) {
+      this.flipCardAnimation(card);
+      if (!this.firstCard) {
+        this.firstCard = card;
+        this.firstCardValue = card.getAttribute("data-card-value");
+      } else {
+        this.secondCard = card;
+        let secondCardValue = card.getAttribute("data-card-value");
+        if (this.firstCardValue == secondCardValue) {
+          this.firstCard.classList.add("matched");
+          this.secondCard.classList.add("matched");
+          this.firstCard = false;
+          this.matchedPairsCount += 1;
+
+          if (this.matchedPairsCount == Math.floor(this.numOfCards / 2)) {
+            result.innerHTML = `<h2>You Won</h2>`;
+            stopGame();
+          }
+        } else {
+          let [tempFirst, tempSecond] = [this.firstCard, this.secondCard];
+          this.firstCard = false;
+          this.secondCard = false;
+
+          setTimeout(() => {
+            this.flipCardAnimation(tempFirst);
+            this.flipCardAnimation(tempSecond);
+          }, 800);
+        }
+      }
+    }
+  };
+}
+
 class MatchGrid {
   constructor(args) {
     this.widthAndHeight = args.widthAndHeight;
-    this.numOfColumnsAndRows = args.numOfColumnsAndRows;
-    this.timeLimit = args.timeLimit;
+    this.numOfColumns = args.numOfColumns;
+    this.numOfRows = args.numOfRows;
+    this.seconds = args.timeLimit % 60;
+    this.minutes = Math.floor(args.timeLimit / 60);
     this.themeColor = args.themeColor;
     this.fontSize = args.fontSize;
+    this.card = new Card(args.numOfColumns * args.numOfRows);
   }
 
-  seconds = 0;
-  minutes = 0;
   isGameStarted = false;
   isGamePaused = false;
-  firstCard = false;
-  secondCard = false;
-  winCount = 0;
   isCursorOut = false;
+  gameEventListener = null;
+
+  convetAndSetTimeValues(seconds, minutes, DOMElement) {
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    DOMElement.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}`;
+    return;
+  }
 
   timeGenerator() {
-    this.seconds += 1;
+    this.seconds -= 1;
 
-    if (this.seconds >= 60) {
-      this.minutes += 1;
-      this.seconds = 0;
+    if ((this.seconds % 60) + 1 === 0) {
+      this.minutes -= 1;
+      this.seconds += 60;
     }
 
-    let secondsValue = this.seconds < 10 ? `0${this.seconds}` : this.seconds;
-    let minutesValue = this.minutes < 10 ? `0${this.minutes}` : this.minutes;
-    timeValue.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}`;
+    this.convetAndSetTimeValues(this.seconds, this.minutes, timeValue);
 
-    if (this.seconds === this.timeLimit) {
+    if (this.minutes === 0 && this.seconds === 0) {
       result.innerHTML = `<h2>You Lose</h2>`;
       this.stopGame();
     }
   }
 
   generateRandom() {
-    let arrayWithNums = Array.from(Array(this.numOfColumnsAndRows * 2).keys());
+    let arrayWithNums = Array.from(
+      Array((this.numOfColumns * this.numOfRows) / 2).keys()
+    );
     arrayWithNums = arrayWithNums.concat(arrayWithNums);
 
     let currentIndex = arrayWithNums.length,
@@ -86,7 +158,7 @@ class MatchGrid {
 
   matrixGenerator(cardValues) {
     gameContainer.innerHTML = "";
-    for (let i = 0; i < this.numOfColumnsAndRows * 4; i++) {
+    for (let i = 0; i < this.numOfColumns * this.numOfRows; i++) {
       gameContainer.innerHTML += `
       <div class="card-container" data-card-value="${cardValues[i]}">
         <div class="card-before" style="background-color: ${args.themeColor};">?</div>
@@ -95,77 +167,21 @@ class MatchGrid {
       `;
     }
 
-    gameContainer.style.gridTemplateColumns = `repeat(${this.numOfColumnsAndRows},auto)`;
-    cards = document.querySelectorAll(".card-container");
-    cards.forEach((card) => {
-      card.addEventListener("click", () => {
-        if (card.classList.contains("flipped")) return;
-        if (!card.classList.contains("matched")) {
-          anime({
-            targets: card,
-            scale: [{ value: 1 }, { value: 1 }, { value: 1, delay: 250 }],
-            rotateY: { value: "+=180", delay: 200 },
-            easing: "easeInOutSine",
-            duration: 400,
-            complete: function () {
-              card.classList.add("flipped");
-            },
-          });
-          if (!this.firstCard) {
-            this.firstCard = card;
-            this.firstCardValue = card.getAttribute("data-card-value");
-          } else {
-            this.secondCard = card;
-            let secondCardValue = card.getAttribute("data-card-value");
-            if (this.firstCardValue == secondCardValue) {
-              this.firstCard.classList.add("matched");
-              this.secondCard.classList.add("matched");
-              this.firstCard = false;
-              this.winCount += 1;
-
-              if (this.winCount == Math.floor(cardValues.length / 2)) {
-                result.innerHTML = `<h2>You Won</h2>`;
-                this.stopGame();
-              }
-            } else {
-              let [tempFirst, tempSecond] = [this.firstCard, this.secondCard];
-              this.firstCard = false;
-              this.secondCard = false;
-
-              let delay = setTimeout(() => {
-                anime({
-                  targets: tempFirst,
-                  scale: [{ value: 1 }, { value: 1 }, { value: 1, delay: 250 }],
-                  rotateY: { value: "+=180", delay: 200 },
-                  easing: "easeInOutSine",
-                  duration: 400,
-                  complete: function () {
-                    tempFirst.classList.remove("flipped");
-                  },
-                });
-
-                anime({
-                  targets: tempSecond,
-                  scale: [{ value: 1 }, { value: 1 }, { value: 1, delay: 250 }],
-                  rotateY: { value: "+=180", delay: 200 },
-                  easing: "easeInOutSine",
-                  duration: 400,
-                  complete: function () {
-                    tempSecond.classList.remove("flipped");
-                  },
-                });
-              }, 800);
-            }
-          }
-        }
-      });
-    });
+    gameContainer.style.gridTemplateColumns = `repeat(${this.numOfColumns},auto)`;
+    this.gameEventListener = (e) => {
+      this.card.cardOnClickLogic(e, this.stopGame);
+    };
+    gameContainer.addEventListener("click", this.gameEventListener);
   }
 
   replayGame() {
-    timeValue.innerHTML = `<span>Time: </span>00:00`;
-    this.seconds = 0;
-    this.minutes = 0;
+    this.seconds = args.timeLimit % 60;
+    this.minutes = Math.floor(args.timeLimit / 60);
+    this.convetAndSetTimeValues(this.seconds, this.minutes, timeValue);
+    this.card.matchedPairsCount = 0;
+    this.seconds = args.timeLimit % 60;
+    this.minutes = Math.floor(args.timeLimit / 60);
+    gameContainer.removeEventListener("click", this.gameEventListener);
     clearInterval(this.interval);
     this.startGame();
   }
@@ -186,30 +202,25 @@ class MatchGrid {
 
   startGame() {
     this.isGameStarted = true;
-    this.winCount = 0;
+    this.card.matchedPairsCount = 0;
     controls.classList.add("hide");
-    stopButton.classList.remove("hide");
-    replayButton.classList.remove("hide");
-    startButton.classList.add("hide");
     let cardValues = this.generateRandom();
     this.interval = setInterval(matchingGame.timeGenerator.bind(this), 1000);
     this.matrixGenerator(cardValues);
     this.isCursorOut = false;
   }
 
-  stopGame() {
+  stopGame = () => {
+    this.seconds = args.timeLimit % 60;
+    this.minutes = Math.floor(args.timeLimit / 60);
     this.isGameStarted = false;
     this.isGamePaused = false;
+    this.card.matchedPairsCount = 0;
     controls.classList.remove("hide");
-    stopButton.classList.add("hide");
-    replayButton.classList.add("hide");
-    startButton.classList.remove("hide");
-
-    timeValue.innerHTML = `<span>Time: </span>00:00`;
-    this.seconds = 0;
-    this.minutes = 0;
+    this.convetAndSetTimeValues(this.seconds, this.minutes, timeValue);
+    gameContainer.removeEventListener("click", this.gameEventListener);
     clearInterval(this.interval);
-  }
+  };
 }
 
 const matchingGame = new MatchGrid(args);
